2d69340796b80c98588c7344c068cda6eec8e20f
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-06-30 09:55:40 -0500

    Defined array of six pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@ alert('hello world!');
 
 let favoriteFood = 'Lasagna!';
 document.write(favoriteFood);
+
+//start 1.2 task
+let pokemonList = [
+  {name: 'Jigglypuff', type: ['fairy', 'normal'], height: 0.5},
+  {name: 'Liepard', type: 'dark', height: 1.1},
+  {name: 'Hypno', type: 'psychic', height: 1.6},
+  {name: 'Palkia', type: ['dragon', 'water'], height: 4.2},
+  {name: 'Muk', type: 'poison', height: 1.2},
+  {name: 'Rattata', type: ['normal', 'dark'], height: 0.3}
+]
