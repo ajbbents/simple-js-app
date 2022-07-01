25eac14ea26cb3f20bc3c12ca24fed038531a875
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-01 11:51:43 -0500

    Reordered object array to alphabetize by name

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //start 1.2 task
 let pokemonList = [
+  {name: 'Hypno', type: 'psychic', height: 1.6},
   {name: 'Jigglypuff', type: ['fairy', 'normal'], height: 0.5},
   {name: 'Liepard', type: 'dark', height: 1.1},
-  {name: 'Hypno', type: 'psychic', height: 1.6},
-  {name: 'Palkia', type: ['dragon', 'water'], height: 4.2},
   {name: 'Muk', type: 'poison', height: 1.2},
+  {name: 'Palkia', type: ['dragon', 'water'], height: 4.2},
   {name: 'Rattata', type: ['normal', 'dark'], height: 0.3}
-]
+];
