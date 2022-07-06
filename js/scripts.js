de3ed537bb93e9e0da2443b99155ea8a996c6e50
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-06 09:47:45 -0500

    Adjusted text spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //start 1.2 task
 let pokemonList = [
   {name: 'Hypno', type: 'psychic', height: 1.6},
-  {name: 'Jigglypuff', type: ['fairy', 'normal'], height: 0.5},
+  {name: 'Jigglypuff', type: ['fairy', ' normal'], height: 0.5},
   {name: 'Liepard', type: 'dark', height: 1.1},
   {name: 'Muk', type: 'poison', height: 1.2},
-  {name: 'Palkia', type: ['dragon', 'water'], height: 4.2},
-  {name: 'Rattata', type: ['normal', 'dark'], height: 0.3}
+  {name: 'Palkia', type: ['dragon', ' water'], height: 4.2},
+  {name: 'Rattata', type: ['normal', ' dark'], height: 0.3}
 ];
 
 //wanting my list to appear with each iteration on a new line, but...
