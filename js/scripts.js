d7f6fe53a338c6578aa90c3f5dda539834b619b7
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-06 11:55:43 -0500

    Added IIFE functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
 //start 1.2 task
-let pokemonList = [
-  {name: 'Hypno', type: 'psychic', height: 1.6},
-  {name: 'Jigglypuff', type: ['fairy', ' normal'], height: 0.5},
-  {name: 'Liepard', type: 'dark', height: 1.1},
-  {name: 'Muk', type: 'poison', height: 1.2},
-  {name: 'Palkia', type: ['dragon', ' water'], height: 4.2},
-  {name: 'Rattata', type: ['normal', ' dark'], height: 0.3}
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Hypno', type: 'psychic', height: 1.6},
+    {name: 'Jigglypuff', type: ['fairy', ' normal'], height: 0.5},
+    {name: 'Liepard', type: 'dark', height: 1.1},
+    {name: 'Muk', type: 'poison', height: 1.2},
+    {name: 'Palkia', type: ['dragon', ' water'], height: 4.2},
+    {name: 'Rattata', type: ['normal', ' dark'], height: 0.3}
+  ];
 
-pokemonList.forEach(function(user) {
-  document.write(user.name + ": (height: " + user.height + ") " + user.type + "<br>");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+    return pokemonList;
+  };
+
+  function getAll() {
+    return pokemonList;
+  };
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.add();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + ": (height: " + pokemon.height + ") " + pokemon.type + "<br>");
 });
 
 /*for (let i=0; i<pokemonList.length; i++){
