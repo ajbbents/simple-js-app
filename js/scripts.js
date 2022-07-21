8202e587371a40531d11d99afdd4fe7809fb7411
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-21 10:17:31 -0500

    added list loading function

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,23 @@ let pokemonRepository = (function () {
     })
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
   return {
     add: add,
     getAll: getAll,
