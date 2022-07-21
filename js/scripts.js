7a2696d25c5671b620f59095d02678392f8bd660
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-21 10:15:35 -0500

    removed pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Hypno', type: 'psychic', height: 1.6},
+  let pokemonList = [];
+/*  {name: 'Hypno', type: 'psychic', height: 1.6},
     {name: 'Jigglypuff', type: ['fairy', ' normal'], height: 0.5},
     {name: 'Liepard', type: 'dark', height: 1.1},
     {name: 'Muk', type: 'poison', height: 1.2},
     {name: 'Palkia', type: ['dragon', ' water'], height: 4.2},
-    {name: 'Rattata', type: ['normal', ' dark'], height: 0.3}
-  ];
+    {name: 'Rattata', type: ['normal', ' dark'], height: 0.3} */
 
   function add(pokemon) {
     pokemonList.push(pokemon);
