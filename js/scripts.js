775b3c36531c6a11cf42a83e3064b667a37d0398
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-25 15:41:43 -0500

    removed old poke list

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
-/*  {name: 'Hypno', type: 'psychic', height: 1.6},
-    {name: 'Jigglypuff', type: ['fairy', ' normal'], height: 0.5},
-    {name: 'Liepard', type: 'dark', height: 1.1},
-    {name: 'Muk', type: 'poison', height: 1.2},
-    {name: 'Palkia', type: ['dragon', ' water'], height: 4.2},
-    {name: 'Rattata', type: ['normal', ' dark'], height: 0.3} */
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
