b31ff6bad3a2f2f85412b620eafdd83e07cfa367
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-07-28 09:48:19 -0500

    moved modelContainer indicator

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal');
 
   function add(pokemon) {
     pokemonList.push(pokemon);
