2a34e49b0f7c362bca05bf372b1e5cf100549da5
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-08-16 16:01:03 -0500

    Add search code.

--- js/scripts.js
+++ js/scripts.js
@@ -101,23 +101,23 @@ let pokemonRepository = (function() {
 		modalBody.append(abilitiesElement);
 	}
 
-	// const search = document.getElementById('search');
-	// search.addEventListener('input', searchList);
-	//
-	// function searchList() {
-	// 	let searchInput = document.getElementById('search').value;
-	// 	searchInput = searchInput.toLowerCase();
-	// 	const listItems = $('button');
-	// 	listItems.each(function() {
-	// 		const item = $(this);
-	// 		const name = item.text();
-	// 		if (name.includes(searchInput)) {
-	// 			item.show();
-	// 		} else {
-	// 			item.hide();
-	// 		}
-	// 	});
-	// }
+	const search = document.getElementById('search');
+	search.addEventListener('input', searchList);
+
+	function searchList() {
+		let searchInput = document.getElementById('search').value;
+		searchInput = searchInput.toLowerCase();
+		const listItems = $('button');
+		listItems.each(function() {
+			const item = $(this);
+			const name = item.text();
+			if (name.includes(searchInput)) {
+				item.show();
+			} else {
+				item.hide();
+			}
+		});
+	}
 
 	return {
 		add: add,
